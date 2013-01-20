c65e006d1e67dfc5721e70d5837b478dd1b5cac1
Chintan Shah <kathi.shah@gmail.com>
2013-01-19 22:54:23 -0800

    adding gitignore and making sure syntax is correct

--- lib/bpm_detect.rb
+++ lib/bpm_detect.rb
@@ -1,7 +1,9 @@
 module BpmDetect
 	INPUT_BLOCK_SAMPLES     = 2048
 	DECIMATED_BLOCK_SAMPLES = 256
-	AVG_DECAY 				= 0.99986
-	AVG_NORM 				= (1 - AVG_DECAY)
-	RMS_VOLUME_ACCU         = (0.045f * 0.045f) / AVG_NORM; #for float samples
+	AVG_DECAY 				      = 0.99986
+	AVG_NORM 				        = (1 - AVG_DECAY)
+	RMS_VOLUME_ACCU         = (0.045 * 0.045) / AVG_NORM; #for float samples
+  MIN_BPM                 = 29
+  MAX_BPM                 = 200
 end
