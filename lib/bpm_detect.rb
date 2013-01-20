ab9fe9c9ca4de202fce4a982b3fd7eb2fce616aa
Chintan Shah <kathi.shah@gmail.com>
2013-01-19 23:03:27 -0800

    reformating code

--- lib/bpm_detect.rb
+++ lib/bpm_detect.rb
@@ -1,9 +1,9 @@
 module BpmDetect
-	INPUT_BLOCK_SAMPLES     = 2048
-	DECIMATED_BLOCK_SAMPLES = 256
-	AVG_DECAY 				      = 0.99986
-	AVG_NORM 				        = (1 - AVG_DECAY)
-	RMS_VOLUME_ACCU         = (0.045 * 0.045) / AVG_NORM; #for float samples
-  MIN_BPM                 = 29
-  MAX_BPM                 = 200
+    INPUT_BLOCK_SAMPLES     = 2048
+    DECIMATED_BLOCK_SAMPLES = 256
+    AVG_DECAY 				= 0.99986
+    AVG_NORM 				= (1 - AVG_DECAY)
+    RMS_VOLUME_ACCU         = (0.045 * 0.045) / AVG_NORM; #for float samples
+    MIN_BPM                 = 29
+    MAX_BPM                 = 200
 end
--- lib/bpm_detect/detector.rb
+++ lib/bpm_detect/detector.rb
@@ -1,31 +1,31 @@
 module BpmDetect
-	class Detector
-		def initialize(channels, sample_rate)
-			@channels = channels
-			@sample_rate = sample_rate
-			@decimate_sum = 0
-			@decimate_count = 0
-			@envelope_accu = 0
+    class Detector
+        def initialize(channels, sample_rate)
+            @channels = channels
+            @sample_rate = sample_rate
+            @decimate_sum = 0
+            @decimate_count = 0
+            @envelope_accu = 0
 
-			# choose decimation factor so that result is approx. 1000 Hz
-			@decimate_by = @sample_rate / 1000
-			raise "Failed: decimate_by > 0" unless @decimate_by > 0
-			raise "Failed: INPUT_BLOCK_SAMPLES < @decimate_by * DECIMATED_BLOCK_SAMPLES" unless INPUT_BLOCK_SAMPLES < @decimate_by * DECIMATED_BLOCK_SAMPLES
+            # choose decimation factor so that result is approx. 1000 Hz
+            @decimate_by = @sample_rate / 1000
+            raise "Failed: decimate_by > 0" unless @decimate_by > 0
+            raise "Failed: INPUT_BLOCK_SAMPLES < @decimate_by * DECIMATED_BLOCK_SAMPLES" unless INPUT_BLOCK_SAMPLES < @decimate_by * DECIMATED_BLOCK_SAMPLES
 
-			# Calculate window length & starting item according to desired min & max bpms
-			@window_len = (60 * @sample_rate) / (@decimate_by * MIN_BPM)
-			@window_start = (60 * @sample_rate) / (@decimate_by * MAX_BPM)
+            # Calculate window length & starting item according to desired min & max bpms
+            @window_len = (60 * @sample_rate) / (@decimate_by * MIN_BPM)
+            @window_start = (60 * @sample_rate) / (@decimate_by * MAX_BPM)
 
-			raise "Failed: @window_len > @window_start" unless @window_len > @window_start
+            raise "Failed: @window_len > @window_start" unless @window_len > @window_start
 
-			# allocate new working objects
-			@xcorr = Array.new(@window_len)
+            # allocate new working objects
+            @xcorr = Array.new(@window_len)
 
-			# allocate processing buffer
-			@buffer = Object.new # new FIFOSampleBuffer();
-			# we do processing in mono mode
-			@buffer.channels = 1
-			@buffer.clear
-		end
-	end
+            # allocate processing buffer
+            @buffer = Object.new # new FIFOSampleBuffer();
+            # we do processing in mono mode
+            @buffer.channels = 1
+            @buffer.clear
+        end
+    end
 end
